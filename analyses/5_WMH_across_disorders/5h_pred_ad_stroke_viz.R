4774e3673b7c489c63e7ff63bfbff7f70bda1379
parent41 <oparent456@gmail.com>
2026-01-14 10:26:28 -0500

    reviews

--- analyses/5_WMH_across_disorders/5h_pred_ad_stroke_viz.R
+++ analyses/5_WMH_across_disorders/5h_pred_ad_stroke_viz.R
@@ -37,9 +37,6 @@ df_noparc_vol = as.data.frame(fread("results/5g_pred_ad_stroke/noparc_vol_metric
 df_noparc_vol$run = "noparc_vol"
 df_noparc_vol = df_noparc_vol %>% mutate(Fold = row_number())
 
-df_prs = as.data.frame(fread("results/5g_pred_ad_stroke/all_prs_metrics.csv"))
-df_prs$run = factor(df_prs$run, levels=c("noparc_vol", "pvdeep_vol", "clust_vol", "noparc_vol_micro", "pvdeep_vol_micro", "clust_vol_micro"))
-
 # Bind and format
 
 df = rbind(df_clust_micro, df_clust_vol, df_pvdeep_micro, df_pvdeep_vol, df_noparc_micro, df_noparc_vol)
@@ -55,17 +52,12 @@ metrics = c("roc_auc", "balanced_accuracy", "f1")
 metrics_clean = c("AUROC", "Balanced accuracy", "F1")
 
 plts = list()
-plts_prs = list()
 
 for (m in 1:length(metrics)) {
 
     df_metric = df %>%
         rename(Metric = all_of(metrics[m])) %>%
         select(Metric, run, Fold)
-
-    df_prs_metric = df_prs %>%
-        rename(Metric = all_of(metrics[m])) %>%
-        select(Metric, run)
 
     df_summary = df_metric %>%
         group_by(run) %>%
@@ -107,26 +99,8 @@ for (m in 1:length(metrics)) {
             )
     ggsave(paste0("./visualization/5g_pred_ad_stroke/",metrics[m],".png"), width=5, height=5)
     print(paste0("./visualization/5g_pred_ad_stroke/",metrics[m],".png"))
-
-    plts_prs[[m]] = ggplot(df_metric, aes(
-            x=run,
-            y=Metric,
-            color = run
-        )) +
-        geom_point(data=df_prs_metric, size=5) +
-        scale_x_discrete(name="", labels=c("No parc (vol)", "PV/deep (vol)", "Cluster (vol)", "No parc (vol + micro)", "PV/deep (vol + micro)", "Clusters (vol + micro)")) + 
-        scale_y_continuous(name=metrics_clean[m]) +
-        scale_color_manual(values = c("#808080", "#d99a9a", "#92a8df", "#000000","#b23535", "#2451bf"), guide="none") +
-        ggtitle(metrics_clean[m]) +
-        theme_light() +
-        theme(
-            axis.text.x = element_text(angle = 30, hjust = 1), text = element_text(size=20),
-            plot.title = element_text(hjust = 0.5)
-            )
-    ggsave(paste0("./visualization/5g_pred_ad_stroke/",metrics[m],"_prs.png"), width=5, height=5)
-    print(paste0("./visualization/5g_pred_ad_stroke/",metrics[m],"_prs.png"))
 }
 
-wrap_plots(c(plts, plts_prs), nrow = 2)
-ggsave(paste0("./visualization/5g_pred_ad_stroke/all_metrics.png"), width=6*length(plts), height=14)
+wrap_plots(c(plts), nrow = 2)
+ggsave(paste0("./visualization/5g_pred_ad_stroke/all_metrics.png"), width=6*length(plts), height=7)
 
